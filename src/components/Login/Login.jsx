d0c9648215f42a961e8903515ab5b2ff0b0e2500
Nikolay <avramov94@abv.bg>
2023-05-02 23:32:59 +0300

    add more stye to login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,31 @@
 import {Link} from "react-router-dom";
 import {AiFillFacebook} from "react-icons/ai";
 import LoginCss from "./Login.module.css";
+import iphoneImg from "../../assets/1.png";
 export function Login() {
 	return (
 		<div className={LoginCss.container}>
+			<img className={LoginCss.image} src={iphoneImg} alt="" />
 			<div className={LoginCss.wrapper}>
-				<h3 className={LoginCss.title}>Minstagram</h3>
-				<div className={LoginCss.inputBox}>
-					<input className={LoginCss.input} type="text" placeholder="Please enter your email here" />
-					<input className={LoginCss.input} type="password" placeholder="Password" />
-					<button className={LoginCss.loginBtn}>Log in</button>
+				<div className={LoginCss.acc}>
+					<h3 className={LoginCss.title}>Minstagram</h3>
+					<div className={LoginCss.inputBox}>
+						<input className={LoginCss.input} type="text" placeholder="Please enter your email here" />
+						<input className={LoginCss.input} type="password" placeholder="Password" />
+						<button className={LoginCss.loginBtn}>Log in</button>
+					</div>
+					<p className={LoginCss.or}>or</p>
+					<div className={LoginCss.alternative}>
+						<AiFillFacebook />
+						<p>Log in with Facebook</p>
+					</div>
+					<Link className={LoginCss.forgotPass}>Forgot password?</Link>
 				</div>
-				<p className={LoginCss.or}>or</p>
-				<p>
-					<AiFillFacebook />
-					Log in with Facebook
-				</p>
-				<Link>Forgot password?</Link>
 
-				<div className="noAcc">
-					<p>Don't have an account? Sign up</p>
+				<div className={LoginCss.noAcc}>
+					<p>
+						Don't have an account? <Link className={LoginCss.signULink}>Sign up</Link>
+					</p>
 				</div>
 			</div>
 		</div>
